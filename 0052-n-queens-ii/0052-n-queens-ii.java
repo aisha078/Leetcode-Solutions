1424bd4ae09e65daf89e7e9c04e6b6c15770adcc
AISHA <aieshavns1530@gmail.com>
2026-07-15 09:42:46 +0530

    Time: 2 ms (64.04%), Space: 42.2 MB (29.39%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,44 @@
+class Solution {
+    int count = 0;
+
+    public int totalNQueens(int n) {
+        char[][] board = new char[n][n];
+
+        for (char[] row : board)
+            java.util.Arrays.fill(row, '.');
+
+        backtrack(board, 0);
+        return count;
+    }
+
+    private void backtrack(char[][] board, int row) {
+        if (row == board.length) {
+            count++;
+            return;
+        }
+
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                backtrack(board, row + 1);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isSafe(char[][] board, int row, int col) {
+        for (int i = 0; i < row; i++)
+            if (board[i][col] == 'Q')
+                return false;
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 'Q')
+                return false;
+
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++)
+            if (board[i][j] == 'Q')
+                return false;
+
+        return true;
+    }
+}
