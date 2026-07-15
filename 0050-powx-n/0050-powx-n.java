bdacab146d84668bc1f7deacacf2114151b3286e
AISHA <aieshavns1530@gmail.com>
2026-07-15 09:41:27 +0530

    Time: 0 ms (100%), Space: 48.4 MB (11.56%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,23 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
+        long N = n;
 
-        long power = n;     // Use long to handle Integer.MIN_VALUE
-
-        if (power < 0) {
+        if (N < 0) {
             x = 1 / x;
-            power = -power;
+            N = -N;
         }
 
-        double ans = 1.0;
+        double ans = 1;
 
-        while (power > 0) {
-
-            if (power % 2 == 1) {   // If power is odd
+        while (N > 0) {
+            if ((N & 1) == 1)
                 ans *= x;
-            }
 
             x *= x;
-            power /= 2;
+            N >>= 1;
         }
 
         return ans;
