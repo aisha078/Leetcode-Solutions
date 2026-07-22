a369eb2d55a8ef9de74736d40d25c5353494fb34
AISHA <aieshavns1530@gmail.com>
2026-07-22 21:59:28 +0530

    Time: 1 ms (100%), Space: 46.6 MB (29.93%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -1,58 +1,28 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
 
-    TreeNode first = null;
-    TreeNode second = null;
-    TreeNode prev = null;
+    TreeNode first = null, second = null, prev = null;
 
     public void recoverTree(TreeNode root) {
         inorder(root);
 
-        // Swap the values of the two incorrect nodes
         int temp = first.val;
         first.val = second.val;
         second.val = temp;
     }
 
     private void inorder(TreeNode node) {
+        if (node == null) return;
 
-        // Base case
-        if (node == null)
-            return;
-
-        // Traverse left subtree
         inorder(node.left);
 
-        // Check for BST property violation
         if (prev != null && prev.val > node.val) {
-
-            // First violation
-            if (first == null) {
+            if (first == null)
                 first = prev;
-            }
-
-            // Update second node
             second = node;
         }
 
-        // Update previous node
         prev = node;
 
-        // Traverse right subtree
         inorder(node.right);
     }
 }
